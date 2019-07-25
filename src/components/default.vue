959d6a9322153fe84dd6af1963acca3cfa1d4632
jtfl2008 <jtfl2008@gmail.com>
2019-07-25 17:36:48 +0800

    添加 默认组件

---
+++ src/components/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    propName: {
+      type: Number,
+      default: 0
+    }
+  },
+  components: {},
+  data() {
+    return {
+      //   key: value
+    }
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+}
+</script>
+<style scoped></style>
